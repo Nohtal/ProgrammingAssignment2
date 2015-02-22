26dfa08a9702a467af0eb1dbf0a6bbdc6397bfe5
Nohtal <party.in.the.sky@gmail.com>
2015-02-22 18:00:09 -0500

    Finished programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL #initiating m to be null
+        set<-function(y){
+                x<<-y #taking in a matrix y and placing it in x
+                m<<-NULL #making m NULL once this function is run
+        }
+        get<-function() x #taking the matrix x and putting into the object get
+        invmatrix<-function(solve) m<<- solve # putting solve into invmatrix
+        getmatrix<-function() m #putting in the NULL/matrix of m into getmatrix
+        list(set=set, get=get, #outputting all of the objects in makeCacheMatrix
+             invmatrix=invmatrix,
+             getmatrix=getmatrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+        m<-x$getmatrix() #reading the object 'getmatrix' inside x to put in m
+        if(!is.null(m)){ #testing if m has a matrix or null. if so return the matrix
+                message("getting cached data")
+                return(m)
+        }
+        matrix<-x$get() #when m is null go 'get' the created matrix
+        m<-solve(matrix, ...) #find the inverse of the matrix
+        x$invmatrix(m) # put the inverted matrix into the object invmatrix
+        m #return the inverted matrix
 }
+        
+        
